777adbdc26b43f708260212680c94d2e5b7ac68c
Lorenzo GM <dev@lorenzogm.com>
2022-01-08 14:07:01 +0100

    Create main.js

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ]
+
+    return config
+  },
+}

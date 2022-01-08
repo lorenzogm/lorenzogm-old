64b8a6ac58e5ea12707eb9baf70f3e3476b178bb
Lorenzo GM <dev@lorenzogm.com>
2022-01-08 10:37:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
   eslint: {
     dirs: ["src"],
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };

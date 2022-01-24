f53a5b633ea92dd3f6f6487723bacba2a751f0c8
Lorenzo García Moreno <dev@lorenzogm.com>
2022-01-24 13:36:15 +0100

    fix: getStaticPaths in CategoryPage

--- src/api/getAllDocsCategories.ts
+++ src/api/getAllDocsCategories.ts
@@ -8,7 +8,5 @@ import { FOLDER_PATH } from './data'
 export function getAllDocsCategories(): Array<DocsCategory['name']> {
   const docsCategories = fs.readdirSync(path.join(FOLDER_PATH))
 
-  return docsCategories.map((docsCategory) => {
-    return docsCategory.split(/^[0-9]*-/, 2)[1]
-  })
+  return docsCategories
 }
